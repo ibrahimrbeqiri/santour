a178b1622a649e482ec8fce86a74887b14331cd4
lavdrimademi <lavdrim.ademi@students.hevs.ch>
2017-11-24 10:21:58 +0100

    no message

---
+++ app/src/main/java/com/group4/santour/PodDetails.java
@@ -0,0 +1,13 @@
+package com.group4.santour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PodDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pod_details);
+    }
+}
