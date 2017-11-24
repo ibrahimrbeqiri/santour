2c899e38ea745cbcd6fbb0e21e13e63c72fe2ca5
joeyzuber1 <joey.zuber@students.hevs.ch>
2017-11-24 10:40:59 +0100

    Create Track

---
+++ app/src/main/java/com/group4/santour/CreateTrack.java
@@ -0,0 +1,13 @@
+package com.group4.santour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateTrack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_track);
+    }
+}
