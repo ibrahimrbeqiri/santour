4f798298658307cf27f14d2e8d87f0600acb6261
joeyzuber1 <joey.zuber@students.hevs.ch>
2017-12-01 09:54:10 +0100

    View

--- app/src/main/java/com/group4/santour/CreateTrack.java
+++ app/src/main/java/com/group4/santour/CreateTrack.java
@@ -12,7 +12,6 @@ import android.location.LocationManager;
 import android.os.SystemClock;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
