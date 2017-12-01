857c1b75efbe6921e946b559c7f1fd1badd8629a
Vincent Belet <vbelt1@gmail.com>
2017-12-01 14:26:22 +0100

    fix

--- app/src/main/java/com/group4/santour/CreateTrack.java
+++ app/src/main/java/com/group4/santour/CreateTrack.java
@@ -149,7 +149,6 @@ public class CreateTrack extends FragmentActivity implements OnMapReadyCallback,
 
         EditText trackname = findViewById(R.id.editText);
         trackname.setEnabled(false);
-        trackname.setText("");
 
         distance.setText("Distance: 0.00 km");
 
@@ -179,6 +178,7 @@ public class CreateTrack extends FragmentActivity implements OnMapReadyCallback,
 
         EditText trackname = findViewById(R.id.editText);
         trackname.setEnabled(true);
+        trackname.setText("");
 
         time.stop();
 
@@ -187,13 +187,14 @@ public class CreateTrack extends FragmentActivity implements OnMapReadyCallback,
         long elapsedMinutes = TimeUnit.MILLISECONDS.toMinutes(elapsed);
         long elapsedHours = TimeUnit.MILLISECONDS.toHours(elapsed);
 
-        for(int i = 0; i < points.size(); i++)
-        {
-            LatLng point = points.get(i);
-            options.add(point);
+        if(points.size() > 0) {
+            for (int i = 0; i < points.size(); i++) {
+                LatLng point = points.get(i);
+                options.add(point);
+            }
         }
 
-        gpsTrack = mMap.addPolyline(options);
+        //gpsTrack = mMap.addPolyline(options);
 
         if(locations.size() > 1) {
 
