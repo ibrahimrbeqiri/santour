90acf1e6fa9729c42dbbc0ef63560f6fed02b84c
joeyzuber1 <joey.zuber@students.hevs.ch>
2017-12-15 13:30:37 +0100

    no message

--- app/src/main/java/com/group4/santour/POIPODList.java
+++ app/src/main/java/com/group4/santour/POIPODList.java
@@ -1,6 +1,7 @@
 package com.group4.santour;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
@@ -20,6 +21,8 @@ public class POIPODList extends AppCompatActivity {
 
     private ListView poilistview;
     private ListView podlistview;
+    private ArrayList<POI> poiArrayList;
+    private ArrayList<POD> podArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +30,19 @@ public class POIPODList extends AppCompatActivity {
         setContentView(R.layout.activity_poipodlist);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
-        List<POI> poilist = new ArrayList<>();
-        //poilist = (ArrayList<POD>)getIntent().getSerializableExtra("PoiList");
+        Intent intent = getIntent();
+        poiArrayList = (ArrayList<POI>) intent.getSerializableExtra("poilist");
+        podArrayList = (ArrayList<POD>) intent.getSerializableExtra("podlist");
+
 
         poilistview = (ListView) findViewById(R.id.listpoi);
         podlistview = (ListView) findViewById(R.id.listpod);
         ArrayList listado2 = new ArrayList();
-        listado2.add("uno");
-        listado2.add("dos");
-        listado2.add("tres");
 
-        for(int i=0 ; i < listado2.size(); i++){
-            System.out.print("hahahhahahahhhhhhhhhhaaaaa");
-            System.out.print(listado2.toString());
-        }
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,listado2);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,poiArrayList);
+        ArrayAdapter adapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1,podArrayList);
         poilistview.setAdapter(adapter);
-        podlistview.setAdapter(adapter);
+        podlistview.setAdapter(adapter2);
 
     }
 }
